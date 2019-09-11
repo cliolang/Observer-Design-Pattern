58d45dac76e2a269042f3c5e903295756eca4e20
Clio Lang <54592031+cliolang@users.noreply.github.com>
2019-09-11 17:48:36 -0400

    Delete Subject.java

--- Subject.java
+++
@@ -1,6 +0,0 @@
-//A subject oriented around observer behavior
-public interface Subject {
-	public void registerObserver(Observer observer);
-	public void removeObserver(Observer observer);
-	public void notifyObservers();
-}
